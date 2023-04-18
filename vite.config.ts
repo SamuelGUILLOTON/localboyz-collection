028787a9b8ad2551ff865af07225c6ae1c6d9a4f
Guilloton <samuel.guilloton01@gmail.com>
2023-04-18 23:18:27 +0200

    suppression dir

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      fs: require.resolve('rollup-pligin-node-builtins')
     }
   }
 })
